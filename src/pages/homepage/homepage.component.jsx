87aad41c89bba30b78e399e9b530dd9e180952ce
javierpb275 <javierpb275@gmail.com>
2021-07-25 11:49:16 +0200

    turned pages homepage and result into class components and used state and componentDidMount() for our mock api data.

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import "./homepage.styles.css";
 import DataTable from "../../components/data-table/data-table.component";
-import books from '../../mock-api/books';
+import books from "../../mock-api/books";
 
-const Homepage = () => (
-  <div className="homepage">
-    <h2 className="homepage-title">INTRODUZCA LOS DATOS AQUÍ:</h2>
-    <DataTable books={books}/>
-  </div>
-);
+class Homepage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      books: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ books });
+  }
+
+  render() {
+    const { books } = this.state;
+    return (
+      <div className="homepage">
+        <h2 className="homepage-title">INTRODUZCA LOS DATOS AQUÍ:</h2>
+        <DataTable books={books} />
+      </div>
+    );
+  }
+}
 
 export default Homepage;
--- src/pages/result/result.component.jsx
+++ src/pages/result/result.component.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import "./result.styles.css";
 import ResultTable from "../../components/result-table/result-table.component";
-import sales from '../../mock-api/sales';
+import sales from "../../mock-api/sales";
 
-const Result = () => (
-  <div className="result-page">
-    <h2 className="result-page-title">RESULTADO:</h2>
-    <ResultTable sales={sales}/>
-  </div>
-);
+class Result extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sales: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ sales });
+  }
+
+  render() {
+    const { sales } = this.state;
+    return (
+      <div className="result-page">
+        <h2 className="result-page-title">RESULTADO:</h2>
+        <ResultTable sales={sales} />
+      </div>
+    );
+  }
+}
 
 export default Result;
