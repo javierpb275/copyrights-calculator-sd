1ec48cc47127e1eb036e933c824582458aeb90b4
javierpb275 <javierpb275@gmail.com>
2021-07-25 19:36:06 +0200

    turned datatable and resulttable into class components since they will need state and get props inside render method

--- src/components/data-table/data-table.component.jsx
+++ src/components/data-table/data-table.component.jsx
@@ -1,50 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
 import "./data-table.styles.css";
 import CustomButton from "../custom-button/custom-button.component";
 import CustomInput from "../custom-input/custom-input.component";
 
-const DataTable = ({ books }) => {
-  return (
-    <div className="data-table">
-      <h2 className="data-table-title">DATA TABLE</h2>
-      <div>
-        INTRODUCE EL NOMBRE DEL AUTOR:{" "}
-        <CustomInput
-          inputType={"text"}
-          inputPlaceholder={"Nombre de autor..."}
-          inputName={"nombre-de-autor"}
-        />
+class DataTable extends Component {
+  constructor() {
+    super();
+    this.state = {
+      author: "",
+      percentage: 0,
+      isbns: [],
+      startDate: "",
+      endDate: "",
+    };
+  }
+
+  render() {
+    const { books } = this.props;
+    return (
+      <div className="data-table">
+        <h2 className="data-table-title">DATA TABLE</h2>
+        <div>
+          INTRODUCE EL NOMBRE DEL AUTOR:{" "}
+          <CustomInput
+            inputType={"text"}
+            inputPlaceholder={"Nombre de autor..."}
+            inputName={"nombre-de-autor"}
+          />
+        </div>
+        <div>
+          INTRODUCE EL % DE DERECHOS DE AUTOR PACTADOS:{" "}
+          <CustomInput
+            inputType={"text"}
+            inputPlaceholder={"% pactado..."}
+            inputName={"porcentage-pactado"}
+          />
+          %
+        </div>
+        <div>
+          INTRODUCE LOS ISBN DE LOS LIBROS SOBRE LOS QUE VAS A REALIZAR EL
+          CÁLCULO:{" "}
+          <CustomInput
+            inputType={"text"}
+            inputPlaceholder={"isbn..."}
+            inputName={"isbn"}
+          />
+          <CustomButton buttonName={"AÑADIR"} />
+        </div>
+        <div>INTRODUCE LAS FECHAS DE LA LIQUIDACIÓN: </div>
+        <div>
+          <CustomInput inputType={"date"} inputName={"fecha-origen"} /> A{" "}
+          <CustomInput inputType={"date"} inputName={"fecha-fin"} />
+        </div>
+        <CustomButton buttonName={"CALCULAR"} />
+        {books[0].title}
       </div>
-      <div>
-        INTRODUCE EL % DE DERECHOS DE AUTOR PACTADOS:{" "}
-        <CustomInput
-          inputType={"text"}
-          inputPlaceholder={"% pactado..."}
-          inputName={"porcentage-pactado"}
-        />
-        %
-      </div>
-      <div>
-        INTRODUCE LOS ISBN DE LOS LIBROS SOBRE LOS QUE VAS A REALIZAR EL
-        CÁLCULO:{" "}
-        <CustomInput
-          inputType={"text"}
-          inputPlaceholder={"isbn..."}
-          inputName={"isbn"}
-        />
-        <CustomButton buttonName={"AÑADIR"} />
-      </div>
-      <div>INTRODUCE LAS FECHAS DE LA LIQUIDACIÓN: </div>
-      <div>
-        <CustomInput inputType={"date"} inputName={"fecha-origen"} /> A{" "}
-        <CustomInput inputType={"date"} inputName={"fecha-fin"} />
-      </div>
-      <CustomButton buttonName={"CALCULAR"} />
-      {books.map((book, i) => (
-        <p>{book.title}</p>
-      ))}
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default DataTable;
--- src/components/result-table/result-table.component.jsx
+++ src/components/result-table/result-table.component.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import "./result-table.styles.css";
 
-const ResultTable = ({ sales }) => {
-  return (
-    <div className="result-table">
-      <h2 className="result-table-title">RESULT TABLE</h2>
-      {sales.map((sale, i) => (
-        <p>{sale.quantity}</p>
-      ))}
-    </div>
-  );
-};
+class ResultTable extends Component {
+  constructor() {
+    super();
+    this.state = {
+      rows: [],
+      total: 0,
+    };
+  }
+
+  render() {
+    const { sales } = this.props;
+    return (
+      <div className="result-table">
+        <h2 className="result-table-title">RESULT TABLE</h2>
+        {sales[0].quantity}
+      </div>
+    );
+  }
+}
 
 export default ResultTable;
