7ef5d289c51372ff8a3a1cbbbcac11ba7bfb0c84
javier-pardinas <javierpb275@gmail.com>
2021-07-29 12:06:31 +0200

    created printPdf() function for printing the result-table

--- src/components/result-table/result-table.component.jsx
+++ src/components/result-table/result-table.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./result-table.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
 
 class ResultTable extends Component {
   constructor(props) {
@@ -10,53 +11,76 @@ class ResultTable extends Component {
     };
   }
 
+  printPdf = (id) => {
+    let prtContent = document.getElementById(id);
+    let WinPrint = window.open(
+      "",
+      "",
+      "left=0,top=0,width=1000,height=1200,toolbar=0,scrollbars=0,status=0"
+    );
+    WinPrint.document.write(prtContent.innerHTML);
+    WinPrint.document.close();
+    WinPrint.focus();
+    WinPrint.print();
+    WinPrint.close();
+  };
+
   render() {
     const { tableData } = this.props;
     return (
-      <div className="result-table">
-        <div className="author-dates-table-container">
+      <div>
+        <div id="result-table">
+          <div className="author-dates-table-container">
+            <table className="table-results">
+              <tr className="table-r-results">
+                <th className="table-h-results">AUTOR</th>
+                <th className="table-h-results">PERIODO DE LIQUIDACIÓN</th>
+              </tr>
+              <tr className="table-r-results">
+                <td className="table-d-results">{tableData.authorName}</td>
+                <td className="table-d-results">
+                  {tableData.startDate.split("-").reverse().join("/")} A{" "}
+                  {tableData.endDate.split("-").reverse().join("/")}
+                </td>
+              </tr>
+            </table>
+          </div>
+          <h2 className="liquidacion-details">DETALLES DE LA LIQUIDACIÓN:</h2>
           <table className="table-results">
             <tr className="table-r-results">
-              <th className="table-h-results">AUTOR</th>
-              <th className="table-h-results">PERIODO DE LIQUIDACIÓN</th>
+              <th className="table-h-results">ISBN</th>
+              <th className="table-h-results">TÍTULO</th>
+              <th className="table-h-results">PRECIO DEL LIBRO</th>
+              <th className="table-h-results">NÚMERO DE VENTAS</th>
+              <th className="table-h-results">PORCENTAGE</th>
+              <th className="table-h-results">DERECHOS DEVENGADOS</th>
             </tr>
-            <tr className="table-r-results">
-              <td className="table-d-results">{tableData.authorName}</td>
-              <td className="table-d-results">
-                {tableData.startDate.split("-").reverse().join("/")} A{" "}
-                {tableData.endDate.split("-").reverse().join("/")}
-              </td>
-            </tr>
+            {tableData.selectedProducts.map((selectedProduct) => (
+              <tr key={selectedProduct.id} className="table-r-results">
+                <td className="table-d-results">
+                  {selectedProduct.referencia}
+                </td>
+                <td className="table-d-results">
+                  {selectedProduct.descripcion}
+                </td>
+                <td className="table-d-results">{selectedProduct.precio}€</td>
+                <td className="table-d-results">{selectedProduct.cantidad}</td>
+                <td className="table-d-results">{tableData.percentage}%</td>
+                <td className="table-d-results">
+                  {selectedProduct.base_imponible *
+                    selectedProduct.cantidad *
+                    (tableData.percentage / 100)}
+                  €
+                </td>
+              </tr>
+            ))}
           </table>
         </div>
-        <h2 className="liquidacion-details">DETALLES DE LA LIQUIDACIÓN:</h2>
-        <table className="table-results">
-          <tr className="table-r-results">
-            <th className="table-h-results">ISBN</th>
-            <th className="table-h-results">TÍTULO</th>
-            <th className="table-h-results">PRECIO DEL LIBRO</th>
-            <th className="table-h-results">NÚMERO DE VENTAS</th>
-            <th className="table-h-results">PORCENTAGE</th>
-            <th className="table-h-results">DERECHOS DEVENGADOS</th>
-          </tr>
-          {tableData.selectedProducts.map((selectedProduct) => (
-            <tr key={selectedProduct.id} className="table-r-results">
-              <td className="table-d-results">{selectedProduct.referencia}</td>
-              <td className="table-d-results">{selectedProduct.descripcion}</td>
-              <td className="table-d-results">
-                {selectedProduct.precio}€
-              </td>
-              <td className="table-d-results">{selectedProduct.cantidad}</td>
-              <td className="table-d-results">{tableData.percentage}%</td>
-              <td className="table-d-results">
-                {selectedProduct.base_imponible *
-                  selectedProduct.cantidad *
-                  (tableData.percentage / 100)}
-                €
-              </td>
-            </tr>
-          ))}
-        </table>
+        <CustomButton
+          buttonName={"PDF"}
+          handleClick={this.printPdf}
+          argument={"result-table"}
+        />
       </div>
     );
   }
