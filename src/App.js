32860675ffd5ac921d6f5ad72e6236bc3e83d675
javierpb275 <javierpb275@gmail.com>
2021-08-01 18:11:25 +0200

    used redirect to redirect to the homepage if no data has been provided

--- src/App.js
+++ src/App.js
@@ -3,26 +3,31 @@ import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import Result from "./pages/result/result.component";
 import Header from "./components/header/header.component";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { setCurrentTableData } from "./redux/table-data/table-data.actions";
 
 class App extends Component {
   render() {
+    const {currentTableData} = this.props;
     return (
       <div className="App">
         <Header />
         <Switch>
           <Route exact path="/" render={() => <Homepage />} />
-          <Route path="/resultado" render={() => <Result />} />
+          <Route exact path="/resultado" render={() => currentTableData? (<Result />) : (<Redirect to='/'/>)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapstateToProps = ({ tableData }) => ({
+  currentTableData: tableData.currentTableData
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentTableData: (tableData) => dispatch(setCurrentTableData(tableData)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapstateToProps, mapDispatchToProps)(App);
