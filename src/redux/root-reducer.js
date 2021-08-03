3ff37edfbe6ce819c4ebb381099737943f956b34
javier-pardinas <javierpb275@gmail.com>
2021-08-03 12:22:42 +0200

    implemented configuration of redux-persist and used persistgate for localstorage.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,18 @@
 import { combineReducers } from "redux";
 import tableDataReducer from "./table-data/table-data.reducer";
 import productReducer from "./product/product.reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["tableData", "product"],
+};
+
+const rootReducer = combineReducers({
   tableData: tableDataReducer,
-  product: productReducer
+  product: productReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
