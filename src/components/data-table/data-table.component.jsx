75eeeaa6354649a58332573622a9b8c82b7a9b2e
javier-pardinas <javierpb275@gmail.com>
2021-08-10 12:20:59 +0200

    turned data-table into a functional componente and implemented useState hook for the isbn

--- src/components/data-table/data-table.component.jsx
+++ src/components/data-table/data-table.component.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./data-table.styles.scss";
 import CustomButton from "../custom-button/custom-button.component";
 import CustomInput from "../custom-input/custom-input.component";
@@ -34,16 +34,11 @@ import {
 
 const CustomOrderListWithLoading = WithLoading(CustomOrderList);
 
-class DataTable extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isbn: "",
-    };
-  }
+const DataTable = (props) => {
+  const [isbn, setIsbn] = useState("");
 
-  addProductByIsbn = async (isbn) => {
-    const { addItem, fetchProductsAsync } = this.props;
+  const addProductByIsbn = async (isbn) => {
+    const { addItem, fetchProductsAsync } = props;
 
     const products = await fetchProductsAsync();
 
@@ -56,29 +51,28 @@ class DataTable extends Component {
         "No se ha encontrado el libro que hace referencia al ISBN introducido."
       );
     } else {
-      //foundProduct.cantidad = 100;
       addItem(foundProduct);
     }
   };
 
-  handleChange = (e) => {
-    const { setAuthorName, setPercentage } = this.props;
+  const handleChange = (e) => {
+    const { setAuthorName, setPercentage } = props;
     if (e.target.name === "authorName") {
       setAuthorName(e.target.value);
     } else if (e.target.name === "percentage") {
       setPercentage(e.target.value);
     } else {
-      this.setState({ [e.target.name]: e.target.value });
+      setIsbn(e.target.value);
     }
   };
 
-  onDatesChange = (e) => {
-    const { setStartDate, setEndDate } = this.props;
-    if (this.props.selectedProducts.length) {
+  const onDatesChange = (e) => {
+    const { setStartDate, setEndDate } = props;
+    if (props.selectedProducts.length) {
       if (e.target.name === "startDate") {
-        e.target.value = this.props.startDate;
+        e.target.value = props.startDate;
       } else if (e.target.name === "endDate") {
-        e.target.value = this.props.endDate;
+        e.target.value = props.endDate;
       }
       return alert("No puede cambiar las fechas una vez añadido un ISBN");
     } else {
@@ -90,109 +84,102 @@ class DataTable extends Component {
     }
   };
 
-  render() {
-    const { isbn } = this.state;
-    const {
-      history,
-      match,
-      selectedProducts,
-      setCurrentTableData,
-      selectedProductsCount /* dispatch */,
-      isProductsFetching,
-      authorName,
-      percentage,
-      startDate,
-      endDate,
-    } = this.props; //dispatch automatically passed if we do not provide mapDispatchToProps in connect()
-    return (
-      <div className="data-table">
-        <table className="table-data">
-          <tr className="table-r-data">
-            INTRODUCE EL NOMBRE DEL AUTOR:{" "}
-            <CustomInput
-              inputType={"text"}
-              inputPlaceholder={"Nombre de autor..."}
-              inputName={"authorName"}
-              inputValue={authorName}
-              handleChange={this.handleChange}
-            />
-          </tr>
-          <tr className="table-r-data">
-            INTRODUCE EL % DE DERECHOS DE AUTOR PACTADOS:{" "}
-            <CustomInput
-              inputType={"text"}
-              inputPlaceholder={"% pactado..."}
-              inputName={"percentage"}
-              inputValue={percentage}
-              handleChange={this.handleChange}
-            />
-            %
-          </tr>
-          <tr className="table-r-data">
-            INTRODUCE LAS FECHAS DE LA LIQUIDACIÓN:
-            <CustomInput
-              inputType={"date"}
-              inputName={"startDate"}
-              inputValue={startDate}
-              handleChange={this.onDatesChange}
-            />{" "}
-            A{" "}
-            <CustomInput
-              inputType={"date"}
-              inputName={"endDate"}
-              inputValue={endDate}
-              handleChange={this.onDatesChange}
-            />
-          </tr>
-          <tr className="table-r-data">
-            INTRODUCE LOS ISBN DE LOS LIBROS SOBRE LOS QUE VAS A REALIZAR EL
-            CÁLCULO:
-            <CustomInput
-              inputType={"text"}
-              inputPlaceholder={"isbn..."}
-              inputName={"isbn"}
-              handleChange={this.handleChange}
-            />
-            <CustomButton
-              buttonName={"AÑADIR"}
-              handleClick={this.addProductByIsbn}
-              argument={isbn}
-            />
-            <Scroll>
-              <CustomOrderListWithLoading
-                isLoading={isProductsFetching}
-                items={selectedProducts}
-              />
-            </Scroll>
-          </tr>
-          <tr className="table-r-data">
-            NÚMERO DE LIBROS AÑADIDOS: {selectedProductsCount}
-          </tr>
-        </table>
-        <div
-          className="calculate-button"
-          onClick={() => history.push(`${match.url}resultado`)}
-        >
-          <CustomButton
-            buttonName={"CALCULAR"}
-            /* onClick={() => {
-              history.push(`${match.url}resultado`);
-              dispatch(setCurrentTableData({authorName, percentage, selectedProducts, startDate, endDate}))
-            }} */
-            handleClick={setCurrentTableData}
-            argument={{
-              authorName,
-              percentage,
-              selectedProducts,
-              startDate,
-              endDate,
-            }}
+  const {
+    history,
+    match,
+    selectedProducts,
+    setCurrentTableData,
+    selectedProductsCount,
+    isProductsFetching,
+    authorName,
+    percentage,
+    startDate,
+    endDate,
+  } = props;
+  return (
+    <div className="data-table">
+      <table className="table-data">
+        <tr className="table-r-data">
+          INTRODUCE EL NOMBRE DEL AUTOR:{" "}
+          <CustomInput
+            inputType={"text"}
+            inputPlaceholder={"Nombre de autor..."}
+            inputName={"authorName"}
+            inputValue={authorName}
+            handleChange={handleChange}
           />
-        </div>
+        </tr>
+        <tr className="table-r-data">
+          INTRODUCE EL % DE DERECHOS DE AUTOR PACTADOS:{" "}
+          <CustomInput
+            inputType={"text"}
+            inputPlaceholder={"% pactado..."}
+            inputName={"percentage"}
+            inputValue={percentage}
+            handleChange={handleChange}
+          />
+          %
+        </tr>
+        <tr className="table-r-data">
+          INTRODUCE LAS FECHAS DE LA LIQUIDACIÓN:
+          <CustomInput
+            inputType={"date"}
+            inputName={"startDate"}
+            inputValue={startDate}
+            handleChange={onDatesChange}
+          />{" "}
+          A{" "}
+          <CustomInput
+            inputType={"date"}
+            inputName={"endDate"}
+            inputValue={endDate}
+            handleChange={onDatesChange}
+          />
+        </tr>
+        <tr className="table-r-data">
+          INTRODUCE LOS ISBN DE LOS LIBROS SOBRE LOS QUE VAS A REALIZAR EL
+          CÁLCULO:
+          <CustomInput
+            inputType={"text"}
+            inputPlaceholder={"isbn..."}
+            inputName={"isbn"}
+            handleChange={handleChange}
+          />
+          <CustomButton
+            buttonName={"AÑADIR"}
+            handleClick={addProductByIsbn}
+            argument={isbn}
+          />
+          <Scroll>
+            <CustomOrderListWithLoading
+              isLoading={isProductsFetching}
+              items={selectedProducts}
+            />
+          </Scroll>
+        </tr>
+        <tr className="table-r-data">
+          NÚMERO DE LIBROS AÑADIDOS: {selectedProductsCount}
+        </tr>
+      </table>
+      <div
+        className="calculate-button"
+        onClick={() => history.push(`${match.url}resultado`)}
+      >
+        <CustomButton
+          buttonName={"CALCULAR"}
+          handleClick={setCurrentTableData}
+          argument={{
+            authorName,
+            percentage,
+            selectedProducts,
+            startDate,
+            endDate,
+          }}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   selectedProducts: selectSelectedProducts,
